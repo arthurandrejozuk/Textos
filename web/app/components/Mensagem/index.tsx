7879c8bc595bc38c3f46e8c5653c0f13c54fbdc9
Arthur Avila <arthurpro295@gmail.com>
2024-10-29 10:00:20 -0300

    update

---
+++ web/app/components/Mensagem/index.tsx
@@ -0,0 +1,34 @@
+import Box from "@art/default/Box";
+import Text from "@art/default/Text";
+import styled from "styled-components";
+
+const MensagemStyled = styled(Box)`
+  position: fixed;
+  bottom: 12%;
+  right: 12%;
+  font-size: 40px;
+
+  .mensagem__sucesso {
+    background-color: #63d663a0;
+    color: green;
+    padding: 12px;
+    border-radius: 12px;
+  }
+
+  .mensagem__falha {
+    background-color: red;
+    color: red;
+    padding: 12px;
+    border-radius: 12px;
+  }
+`;
+
+export default function Mensagem({ tipo }: { tipo: string }) {
+  return (
+    <MensagemStyled>
+      <Text className={`mensagem__${tipo}`}>
+        {tipo.charAt(0).toUpperCase() + tipo.slice(1)}
+      </Text>
+    </MensagemStyled>
+  );
+}
